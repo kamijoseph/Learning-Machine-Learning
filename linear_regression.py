8de177991b6c28ddf91095c5f88ed483a9b7cc67
Kami Joseph <josephkiarie561@gmail.com>
2025-01-06 15:25:10 +0300

    Init

--- linear_regression.py
+++ linear_regression.py
@@ -1,1 +1,14 @@
+
 # Simple Linear Regression
+import numpy as np
+import matplotlib.pyplot as plt
+
+class LinearRegression:
+    def __init__(self, learning_rate=0.01, n_iterations=1000):
+        self.learning_rate = learning_rate
+        self.n_iterations = n_iterations
+        self.weights = None
+        self.bias = None
+        
+    def fit(self, X, Y):
+        pass
