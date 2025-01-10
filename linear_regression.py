80ba18f58db9260c70f6f81cdf73b57674dc6101
Kami Joseph <josephkiarie561@gmail.com>
2025-01-10 17:25:09 +0300

    LinearRegression class initialized

--- linear_regression.py
+++ linear_regression.py
@@ -1,2 +1,16 @@
 
 # Linear Regression Model Implementation From Scratch
+import numpy as np
+
+class LinearRegression:
+    def __init__(self, learning_rate=0.01, n_iterations=1000):
+        self.learning_rate = learning_rate
+        self.n_iterations = n_iterations
+        weights = None
+        bias = None
+    
+    def fit(self, X, y):
+        pass
+    
+    def predict(self, X):
+        pass
