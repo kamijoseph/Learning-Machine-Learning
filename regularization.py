b38c918f805c858f2a94361761f35e3ce9e5c473
Kami Joseph <josephkiarie561@gmail.com>
2025-01-13 15:24:24 +0300

    regularization class initialized and parameters defined

--- regularization.py
+++ regularization.py
@@ -1,4 +1,10 @@
 
 # Trying to create regularization functions from scratch.
+import pandas as pd
+
 class Regularization:
-    pass
+    # alpha = regularization strength nd ration is the ration of l1 to l2 in an elasic net case.
+    def __init__(self, regz_type="l2", alpha=0.1, l1_ratio=0.5):
+        self.regz_type = regz_type
+        self.alpha = alpha
+        self.l1_ration = l1_ratio
