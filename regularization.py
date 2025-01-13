06dc040924192d38bcf0f937ef6860184763aa8e
Kami Joseph <josephkiarie561@gmail.com>
2025-01-13 15:49:52 +0300

    L1, L2 and Elastic net penalties compute function

--- regularization.py
+++ regularization.py
@@ -1,10 +1,26 @@
 
 # Trying to create regularization functions from scratch.
-import pandas as pd
+import numpy as np
 
 class Regularization:
     # alpha = regularization strength nd ration is the ration of l1 to l2 in an elasic net case.
-    def __init__(self, regz_type="l2", alpha=0.1, l1_ratio=0.5):
+    def __init__(self, regz_type="l2", alpha=0.1, l1Ratio=0.5):
         self.regz_type = regz_type
         self.alpha = alpha
-        self.l1_ration = l1_ratio
+        self.l1Ratio = l1Ratio
+        
+    # l1 Penalty Function
+    def l1Penalty(self, weights):
+        penalty = np.sum(np.abs(weights))
+        return self.apha * penalty
+    
+    # l2 penalty Function'
+    def l2Penalty(self, weights):
+        penalty = np.sum(weights ** 2)
+        return self.alpha * penalty
+    
+    # Elastic Net Penalty Function
+    def elasticNetPenalty(self, weights):
+        l1Comp = self.l1ratio * self.l1Penalty(weights)
+        l2Comp = (1 - self.l1Ratio) * self.l2Penalty(weights)
+        return l1Comp + l2Comp
