a1a351375b956500121568acb14b2180eb8c15ea
Kami Joseph <josephkiarie561@gmail.com>
2025-01-14 08:33:51 +0300

    support vector machine from scratch

---
+++ SupportVectorMachine.py
@@ -0,0 +1,14 @@
+
+# Simple Support Vector Macine Classifier for 2-Dimensional Data from Scratch.
+import numpy
+import matplotlib.pyplot as plt
+
+class SupportVectorMachine:
+    def __init__(self):
+        pass
+    
+    def fit(self):
+        pass
+    
+    def predict(self):
+        pass
