c15603284ea0c73e1002909db43143c838e9f029
Kami Joseph <josephkiarie561@gmail.com>
2025-01-14 15:24:47 +0300

    standardize the data funcion

--- PrincipalComponentAnalysis.py
+++ PrincipalComponentAnalysis.py
@@ -4,3 +4,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #synthetic data, and features init
+np.random.seed(42)
+mean = [0, 0]
+covariance = [[1, 0.8], [0.8, 1]]
+X = np.random.multivariate_normal(mean, covariance, 100)
+
+#Standardizing data....................
+def standardizeData(data):
+    mean = np.mean(data, axis=0)
+    std = np.std(data, axis=0)
+    return (data - mean) / std
+x_standardized = standardizeData(X)
