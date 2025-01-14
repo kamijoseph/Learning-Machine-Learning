eacd73e43398bf53397a31a5a3f7a201d207397c
Kami Joseph <josephkiarie561@gmail.com>
2025-01-14 09:08:03 +0300

    Fit function for trining

--- SupportVectorMachine.py
+++ SupportVectorMachine.py
@@ -1,18 +1,30 @@
 
 # Simple Support Vector Macine Classifier for 2-Dimensional Data from Scratch.
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
 
 class SupportVectorMachine:
-    def __init__(self, learningRate=0.001, lambdaParameter=0.01, n_iterations =1000):
+    def __init__(self, learningRate=0.001, lambdaParameter=0.01, n_iterations=1000):
         self.learningRate = learningRate
         self.lambdaParameter = lambdaParameter
         self.n_iterations = n_iterations
         self.weights = None
         self.bias = None
         
-    def fit(self):
-        pass
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+        self.weights = np.zeros(n_features)
+        self.bias = 0
+        
+        # labels --> +1 and -1
+        y_ = np.where(y <= 0, -1, 1)
+        
+        for _ in range(self.n_iterations):
+            for idx, x_i in enumerate(X):
+                condition = y_[idx] * (np.dot(x_i, self.weights) - self.bias) >= 1
+                if condition:
+                    self.weights = self.learningRate * (2 * self.lambdaParameter * self.weights)
+                else:
     
     def predict(self):
         pass
