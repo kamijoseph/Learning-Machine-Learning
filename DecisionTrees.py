c291086341c23377321fe5f26b0a93bd684855ee
Kami Joseph <josephkiarie561@gmail.com>
2025-01-15 15:22:56 +0300

    Decision Tree Algorithm initialized

---
+++ DecisionTrees.py
@@ -0,0 +1,33 @@
+
+# Decision trees algorithm from scratch
+import numpy as np
+from collections import Counter
+
+class DecisionTrees:
+    def __init__(self, maxDepth=None):
+        pass
+    
+    class Node:
+        def __init__(self):
+            pass
+    
+    def gini(self):
+        pass
+    
+    def split(self):
+        pass
+    
+    def bestSplit(self):
+        pass
+    
+    def buildTree(self):
+        pass
+    
+    def fit(self):
+        pass
+    
+    def traverseTree(self):
+        pass
+    
+    def predict(self):
+        pass
