a9589e93efca300c78c6747efb4e33be44d57ab8
Kami Joseph <josephkiarie561@gmail.com>
2025-01-20 16:26:51 +0300

    K-nearest neighbour model

---
+++ KNN.py
@@ -0,0 +1,14 @@
+
+# Simple K-Nearest Neighbours algorithm
+class KNN:
+    def __init__(self):
+        pass
+    
+    def fit(self, X, y):
+        pass
+    
+    def predict(self, X):
+        pass
+    
+    def predictor(self, x):
+        pass
