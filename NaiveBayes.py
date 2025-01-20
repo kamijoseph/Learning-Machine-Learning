ee464592425a51bd5ae89b3a67b0f6e8862fcf4e
Kami Joseph <josephkiarie561@gmail.com>
2025-01-20 12:39:31 +0300

    naive bayes classifier initialized

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -1,2 +1,20 @@
 
 # Naive Bayes Algorithm with Numpy from Scratch.
+import numpy as np
+from collections import defaultdict
+
+class NaiveBayes:
+    def __init__(self):
+        pass
+    
+    def fit(self):
+        pass
+    
+    def calculateLikelihood(self):
+        pass
+    
+    def calculatePosterior(self):
+        pass
+    
+    def predict(self):
+        pass
