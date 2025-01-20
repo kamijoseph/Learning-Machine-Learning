5ad0d79e27faa7650ebdc3c42f1e0709508336b2
Kami Joseph <josephkiarie561@gmail.com>
2025-01-20 16:57:50 +0300

    k nearest label counter function

--- KNN.py
+++ KNN.py
@@ -2,6 +2,7 @@
 # Simple K-Nearest Neighbours algorithm
 import numpy as np
 from collections import Counter
+
 class KNN:
     def __init__(self, k=3):
         self.k = k
@@ -15,4 +16,8 @@ class KNN:
         return np.array(predictions)
     
     def predictor(self, x):
-        pass
+        distance = [np.linalg.norm(x-x_train) for x_train in self.X_train]
+        k_indices = np.argsort(distance)[:self.k]
+        k_nearestLabels = [self.y_train[i] for i in k_indices]
+        mostCommon = Counter(k_nearestLabels).most_common(1)
+        return mostCommon[0][0]
