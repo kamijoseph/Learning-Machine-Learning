7643170666e01e43c53a592fc1f250049c779792
Kami Joseph <josephkiarie561@gmail.com>
2025-01-20 16:35:17 +0300

    self, predict, fit and predictor functions for knn model

--- KNN.py
+++ KNN.py
@@ -1,14 +1,18 @@
 
 # Simple K-Nearest Neighbours algorithm
+import numpy as np
+from collections import Counter
 class KNN:
-    def __init__(self):
-        pass
+    def __init__(self, k=3):
+        self.k = k
     
     def fit(self, X, y):
-        pass
+        self.X_train = X
+        self.y_train = y
     
     def predict(self, X):
-        pass
+        predictions = [self.predictor(x) for x in X]
+        return np.array(predictions)
     
     def predictor(self, x):
         pass
