d61f79d16059e2908acfbfc9a259783e97b09105
Kami Joseph <josephkiarie561@gmail.com>
2025-01-20 14:13:48 +0300

    fit function to fit the training to the model

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -9,8 +9,23 @@ class NaiveBayes:
         self.featureLikelihoods = defaultdict(lambda: defaultdict(float))
         self.classes = []
     
-    def fit(self):
-        pass
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+        self.classes = np.unique(y)
+        
+        # calculate the class prior and the feature likelihood
+        for cls in self.classes:
+            self.classPriors[cls] = np.sum(y == cls) / n_samples
+            
+        # feature likelihood per class
+        for cls in self.classes:
+            X_cls = X[y == cls]
+            for featureIdx in range(n_features):
+                featureValues = X_cls[:, featureIdx]
+                mean = np.mean(featureValues)
+                variance = np.var(featureValues)
+                self.featureLikelihoods[cls][featureIdx] = (mean, variance)
+                
     
     def calculateLikelihood(self):
         pass
