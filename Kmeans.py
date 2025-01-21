0e90197124fca5d701fdb936da64bde5e0913217
Kami Joseph <josephkiarie561@gmail.com>
2025-01-21 13:07:12 +0300

    random k indices

--- Kmeans.py
+++ Kmeans.py
@@ -1,5 +1,8 @@
 
 # K Means Clustering Algorithm
+import numpy as np
+import matplotlib.pyplot as plt
+
 class KMeans:
     def __init__(self, n_clusters=3, maxIterations=100, tolerance=1e-4):
         self.n_clusters = n_clusters
@@ -7,8 +10,9 @@ class KMeans:
         self.tolerance = tolerance
         self.centroids = None
     
-    def initializeCentroids(self):
-        pass
+    def initializeCentroids(self, X):
+        indices = np.random.choice(self.X.shape[0], self.n_clusters, replace=False)
+        return X[indices]
     
     def calculateDistance(self):
         pass
