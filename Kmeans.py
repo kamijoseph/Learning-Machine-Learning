f86dbc611d6ed3b338f7585033af7407477223f5
Kami Joseph <josephkiarie561@gmail.com>
2025-01-21 13:35:23 +0300

    fit function

--- Kmeans.py
+++ Kmeans.py
@@ -14,11 +14,22 @@ class KMeans:
         indices = np.random.choice(self.X.shape[0], self.n_clusters, replace=False)
         return X[indices]
     
-    def calculateDistance(self):
-        pass
+    def calculateDistance(self, X, centroids):
+        distances = np.sqrt(((X[:, np.newaxis, :] - centroids) ** 2).sum(axis=2))
+        return distances
     
-    def fit(self):
-        pass
-    
+    def fit(self, X):
+        self.centroids = self.initializeCentroids(X)
+        for iters in range(self.maxIterations):
+            # assign points to the nearest centroid
+            distances = self.calculateDistance(X, self.centroids)
+            clusters = np.argmin(distances, axis=1)
+            # update centroids
+            newCentroids = np.array([X[clusters == k].mean(axis=0) if np.any(clusters == k) else self.centroids[k]
+                                     for k in range(self.n_clusters)])
+            if np.all(np.abs(newCentroids - self.centroids) < self.tolerance):
+                break
+            self.centroids = newCentroids
+        self.clusters = clusters
     def predict(self):
         pass
