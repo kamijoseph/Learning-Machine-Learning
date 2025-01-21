d823b313e408475779c904a207c59c057008c34e
Kami Joseph <josephkiarie561@gmail.com>
2025-01-21 12:53:27 +0300

    k means clustering

---
+++ Kmeans.py
@@ -0,0 +1,17 @@
+
+# K Means Clustering Algorithm
+class KMeans:
+    def __init__(self):
+        pass
+    
+    def initializeCentroids(self):
+        pass
+    
+    def calculateDistance(self):
+        pass
+    
+    def fit(self):
+        pass
+    
+    def predict(self):
+        pass
