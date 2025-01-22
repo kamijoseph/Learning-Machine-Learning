eb8cd232ac99cdcfb612cc6e2041b04f8c9c3ced
Kami Joseph <josephkiarie561@gmail.com>
2025-01-22 13:23:56 +0300

    Initialized args

--- GMM.py
+++ GMM.py
@@ -1,8 +1,14 @@
 
 # Gaussian Mixture Model Implementmentaion from scratch with numpy
 class GaussianMixtureModel:
-    def __init__(self):
-        pass
+    def __init__(self, n_components=1, maxIterations=100, tol=1e-6):
+        self.n_components = n_components
+        self.maxIterations = maxIterations
+        self.tol = tol
+        self.means = None
+        self.covarinces = None
+        self.weights = None
+        
     
     def initializeParameters(self):
         pass
