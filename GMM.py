6d3a26080401b44cfcdbdc67f7294d3b567cdec9
Kami Joseph <josephkiarie561@gmail.com>
2025-01-22 12:46:14 +0300

    gaussian mixture model initialized

---
+++ GMM.py
@@ -0,0 +1,23 @@
+
+# Gaussian Mixture Model Implementmentaion from scratch with numpy
+class GaussianMixtureModel:
+    def __init__(self):
+        pass
+    
+    def initializeParameters(self):
+        pass
+    
+    def gaussianPdf(self):
+        pass
+    
+    def expectationStep(self):
+        pass
+    
+    def maximizationStep(self):
+        pass
+    
+    def fit(self):
+        pass
+    
+    def predict(self):
+        pass
