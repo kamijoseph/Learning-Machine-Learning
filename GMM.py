8123d4e3858a67e54c6c8bab6ebe73af40dbce29
Kami Joseph <josephkiarie561@gmail.com>
2025-01-22 14:00:15 +0300

    initialize parameters function

--- GMM.py
+++ GMM.py
@@ -1,5 +1,7 @@
 
 # Gaussian Mixture Model Implementmentaion from scratch with numpy
+import numpy as np
+
 class GaussianMixtureModel:
     def __init__(self, n_components=1, maxIterations=100, tol=1e-6):
         self.n_components = n_components
@@ -10,10 +12,14 @@ class GaussianMixtureModel:
         self.weights = None
         
     
-    def initializeParameters(self):
-        pass
+    def initializeParameters(self, X):
+        n_samples, n_features = self.X.shape
+        self.weights = np.ones(self.n_components) / self.n_components
+        self.means = X[np.random.choice(n_samples, self.n_components, replace=False)]
+        self.covarinces = np.array([np.eye(n_features) for _ in range(self.n_components)])
+        
     
-    def gaussianPdf(self):
+    def gaussianPdf(self, X, mean, covariance):
         pass
     
     def expectationStep(self):
