83e1cf059006e9205a33eebacd7966dea4aaec7b
Kami Joseph <josephkiarie561@gmail.com>
2025-01-22 14:11:39 +0300

    gaussian pdf function

--- GMM.py
+++ GMM.py
@@ -17,10 +17,13 @@ class GaussianMixtureModel:
         self.weights = np.ones(self.n_components) / self.n_components
         self.means = X[np.random.choice(n_samples, self.n_components, replace=False)]
         self.covarinces = np.array([np.eye(n_features) for _ in range(self.n_components)])
-        
     
     def gaussianPdf(self, X, mean, covariance):
-        pass
+        n_features = X.shape[1]
+        det = np.linalg.det(covariance)
+        inv = np.linalg.det(covariance)
+        normFactor = 1.0 / np.sqrt((2 * np.pi) ** n_features * det)
+        diff = X - mean
     
     def expectationStep(self):
         pass
