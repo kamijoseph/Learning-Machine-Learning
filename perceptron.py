ec1d12b19fe87d6bb2345431104f28d23aff3173
Kami Joseph <josephkiarie561@gmail.com>
2025-02-04 13:42:52 +0300

    constructor function and unit step function

--- perceptron.py
+++ perceptron.py
@@ -7,8 +7,12 @@ def unitStepFunction(x):
 
 
 class Perceptron:
-    def __init__(self):
-        pass
+    def __init__(self, learningRate=0.01, nIterations=1000):
+        self.learningRate = learningRate
+        self.nIterations = nIterations
+        self.activationFunction = unitStepFunction
+        self.weights = None
+        self.bias = None
     
     def fit(self, X, y):
         pass
