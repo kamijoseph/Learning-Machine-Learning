c35393ace4e365295a9cc82afd63398ee04f509e
Kami Joseph <josephkiarie561@gmail.com>
2025-02-04 13:38:35 +0300

    perceptron

--- perceptron.py
+++ perceptron.py
@@ -1,4 +1,11 @@
 
+# implementing a perceptron algoritm in python
+import numpy as np
+
+def unitStepFunction(x):
+    return np.where(x > 0, 1, 0)
+
+
 class Perceptron:
     def __init__(self):
         pass
